c8060866be847b9f2e245d921018f3acb70ca737
reubz <47412059+reubznz@users.noreply.github.com>
2024-01-09 09:02:19 +1300

    Update jellyfin.conf.sh

--- Docker/jellyfin.conf.sh
+++ Docker/jellyfin.conf.sh
@@ -5,7 +5,8 @@
 ContainerName="jellyfin"
 ImageName="lscr.io/linuxserver/jellyfin"
 DockerRunCommand() {
-    docker run -d --name $ContainerName -p 8096:8096 -p 8920:8920 -p 7359:7359/udp -p 1900:1900/udp -e PUID=1000 -e PGID=1000 -e TZ=Pacific/Auckland -e JELLYFIN_PublishedServerUrl=192.168.37.6 -v jellyfin_dat>}
+    docker run -d --name $ContainerName -p 8096:8096 -p 8920:8920 -p 7359:7359/udp -p 1900:1900/udp -e PUID=1000 -e PGID=1000 -e TZ=Pacific/Auckland -e JELLYFIN_PublishedServerUrl=192.168.37.6 -v jellyfin_data:/config -v /media/Media/TV:/data/tvshows -v /media/Media/Movies:/data/movies -v /media/Media/Music:/data/music --device=/dev/dri:/dev/dri --restart unless-stopped $ImageName
+}
 
 echo "" > /tmp/docker_deploy.txt
 docker ps | grep $ContainerName | awk '{print $2,"- Deployed",$4,$5,$6,"- Running for",$8,$9}' > /tmp/docker_deploy.txt
