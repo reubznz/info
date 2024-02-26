a49dce6fac68ca7f95cc0a05a468abbb1191aee6
reubz <47412059+reubznz@users.noreply.github.com>
2024-02-27 10:51:19 +1300

    Create otel-install.ps1

---
+++ Prometheus/otel-install.ps1
@@ -0,0 +1,140 @@
+# Define the script parameters
+
+# Detect the operating system and its architecture
+$OS = "windows"
+$ARCH = $ENV:PROCESSOR_ARCHITECTURE.ToLower()
+$OTEL_VERSION = "0.90.1"
+
+# architecture check
+$ARCH = if ($ARCH -eq "amd64") { "amd64" } elseif ($ARCH -eq "arm64") { "arm64" } elseif ($ARCH -eq "x86") { "386" } else { $ARCH }
+
+# Construct the download URL for otel-collector based on OS and architecture
+# $DOWNLOAD_URL = "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v${OTEL_VERSION}/otelcol-contrib_${OTEL_VERSION}_${OS}_${ARCH}.tar.gz"
+
+$DOWNLOAD_URL = "https://zinc-public-data.s3.us-west-2.amazonaws.com/opentelemetry-collector-releases/otelcol-contrib_${OTEL_VERSION}_${OS}_${ARCH}.zip"
+
+# Download otel-collector from the specified URL
+Invoke-WebRequest -Uri $DOWNLOAD_URL -OutFile "otelcol-contrib.zip"
+
+# Ensure the target directory for extraction exists
+$SERVICE_NAME="otel-collector"
+$directoryPath = "C:\${SERVICE_NAME}\"
+if (-not (Test-Path $directoryPath -PathType Container)) {
+    New-Item -Path $directoryPath -ItemType Directory
+}
+
+# Extract the downloaded archive to the target directory
+# tar -xzf "otelcol-contrib.tar.gz" -C $directoryPath
+Expand-Archive "otelcol-contrib.zip" -DestinationPath $directoryPath -Force
+
+# Generate a sample configuration file for otel-collector
+$ConfigContent = @"
+receivers:
+  hostmetrics:
+    collection_interval: 30s
+    scrapers:
+      cpu:
+      disk:
+      filesystem:
+      load:
+      memory:
+      network:
+      paging:          
+      processes:
+      # process: # a bug in the process scraper causes the collector to throw errors so disabling it for now
+      
+  windowsperfcounters/memory:
+    metrics:
+      bytes.committed:
+        description: the number of bytes committed to memory
+        unit: By
+        gauge:
+    collection_interval: 30s
+    perfcounters:
+      - object: Memory
+        counters:
+          - name: Committed Bytes
+            metric: bytes.committed
+
+  windowsperfcounters/processor:
+    collection_interval: 1m
+    metrics:
+      processor.time:
+        description: active and idle time of the processor
+        unit: "%"
+        gauge:
+    perfcounters:
+      - object: "Processor"
+        instances: "*"
+        counters:
+          - name: "% Processor Time"
+            metric: processor.time
+            attributes:
+              state: active
+      - object: "Processor"
+        instances: [1, 2]
+        counters:
+          - name: "% Idle Time"
+            metric: processor.time
+            attributes:
+              state: idle
+  windowseventlog/application:
+    channel: application
+  windowseventlog/security:
+    channel: security
+  windowseventlog/setup:
+    channel: setup
+  windowseventlog/system:
+    channel: system
+processors:
+  resourcedetection/system:
+    detectors: ["system"]
+    system:
+      hostname_sources: ["os"]
+  memory_limiter:
+    check_interval: 1s
+    limit_percentage: 75
+    spike_limit_percentage: 15
+  batch:
+    send_batch_size: 10000
+    timeout: 10s
+
+extensions:
+  zpages: {}
+  memory_ballast:
+    size_mib: 512
+
+exporters:
+  prometheus:
+    endpoint: "0.0.0.0:55690"
+    send_timestamps: true
+    metric_expiration: 180m
+    enable_open_metrics: true
+    add_metric_suffixes: false
+    resource_to_telemetry_conversion:
+      enabled: true
+
+service:
+  extensions: [zpages, memory_ballast]
+  pipelines:
+    metrics:
+      receivers: [windowsperfcounters/processor, windowsperfcounters/memory, hostmetrics]
+      processors: [resourcedetection/system, memory_limiter, batch]
+      exporters: [prometheus]
+
+"@
+
+# Write the configuration content to a file
+$ConfigContent | Out-File "${directoryPath}otel-config.yaml"
+
+# Define the service parameters
+$params = @{
+  Name           = $SERVICE_NAME
+  BinaryPathName = "${directoryPath}otelcol-contrib.exe --config=${directoryPath}otel-config.yaml"
+  DisplayName    = $SERVICE_NAME
+  StartupType    = "Automatic"
+  Description    = "OpenObserve otel-collector service."
+}
+
+# Create the service
+New-Service @params
